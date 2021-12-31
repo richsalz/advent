7ba5f2b1159906b57cf71a10db210ce657e01a33
Rich Salz <rsalz@akamai.com>
2021-12-30 21:56:38 -0500

    move data & sample files

---
+++ src/bin/d05.rs
@@ -0,0 +1,12 @@
+use advent::utils::Lines;
+use std::env;
+use std::error::Error;
+use std::str::FromStr;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for input in env::args().skip(1) {
+       // part1(&input)?;
+        //part2(&input)?;
+    }
+    Ok(())
+}
