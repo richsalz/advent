4717b8173ca835cd7b36165c73e4d4021739a404
Rich Salz <rsalz@akamai.com>
2022-01-01 12:59:37 -0500

    Keep counts by days, not vector of fish.

--- src/bin/d06.rs
+++ src/bin/d06.rs
@@ -7,25 +7,33 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     for input in env::args().skip(1) {
         part1(&input)?;
-        //part2(&input)?;
+        part2(&input)?;
     }
     Ok(())
 }
 
 fn part1(name: &str) -> Result<(), Box<dyn Error>> {
+    common(name, 80)
+}
+
+fn part2(name: &str) -> Result<(), Box<dyn Error>> {
+    common(name, 256)
+}
+
+fn common(name: &str, days: isize) -> Result<(), Box<dyn Error>> {
     let mut l = Lines::new(name)?;
     l.more();
-    let mut fish: Vec<u8> = l.get().split(',').map(|s| u8::from_str(s).unwrap()).collect();
-    println!("day 0 {:?}", fish);
-    for _days in 0..80 {
-        let new:usize = fish.iter().filter(|&f| *f == 0).count();
-        for f in fish.iter_mut() {
-            if *f == 0 { *f = 6; } else { *f -= 1; }
-        }
-        let mut new: Vec<u8> = vec![8; new];
-        fish.append(&mut new);
-        //println!("day {} {:?}", days, fish);
+    let mut fish: Vec<usize> = vec![0; 9];
+    for num in l.get().split(',').map(|s| usize::from_str(s).unwrap()) {
+        fish[num] += 1;
     }
-    println!("{} fish", fish.len());
+    println!("\nday {} {:?}({}) = {}", 0, fish, fish.len(), fish.iter().sum::<usize>());
+    for _d in 0..days {
+        let new = fish.remove(0);
+        fish.push(new);
+        fish[6] += new;
+        //println!("day {} {:?}({}) = {}", d+1, fish, fish.len(), fish.iter().sum::<usize>());
+    }
+    println!("\nday {} {:?} = {}", days, fish, fish.iter().sum::<usize>());
     Ok(())
 }
