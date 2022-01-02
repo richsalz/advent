e1c4cc4d3d0bd7d8cfe0a640b474de5a4ddce53c
Rich Salz <rsalz@akamai.com>
2022-01-01 22:48:01 -0500

    Refactor, to sart coding part2

--- src/bin/d09.rs
+++ src/bin/d09.rs
@@ -3,17 +3,18 @@ use std::env;
 use std::error::Error;
 
 type Matrix = Vec<Vec<u8>>;
+type Point = (usize, usize);
 
 /// Loop over all arguments and process each one as a file.
 fn main() -> Result<(), Box<dyn Error>> {
     for input in env::args().skip(1) {
         part1(&input)?;
-        //part2(&input)?;
+        part2(&input)?;
     }
     Ok(())
 }
 
-fn part1(input: &str) -> Result<(), Box<dyn Error>> {
+fn load(input: &str) -> Result<Matrix, Box<dyn Error>> {
     let mut l = Lines::new(input)?;
     let mut m: Matrix = Vec::new();
     while l.more() {
@@ -35,14 +36,10 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
             .collect();
         m.push(row);
     }
-
-    let points = findlows(&m);
-    let risks: isize = points.iter().map(|(r, c)| 1 + m[*r][*c] as isize).sum();
-    println!("numrisks {} risks = {}", points.len(), risks);
-    Ok(())
+    Ok(m)
 }
 
-fn findlows(m: &Matrix) -> Vec<(usize, usize)> {
+fn findlows(m: &Matrix) -> Vec<Point> {
     let mut points: Vec<(usize, usize)> = Vec::new();
     let maxrows = m.len() - 1;
     let maxcols = m[0].len() - 1;
@@ -62,4 +59,34 @@ fn findlows(m: &Matrix) -> Vec<(usize, usize)> {
         }
     }
     points
+}
+
+fn part1(input: &str) -> Result<(), Box<dyn Error>> {
+    let m = load(input)?;
+    let points = findlows(&m);
+    let risks: isize = points.iter().map(|(r, c)| 1 + m[*r][*c] as isize).sum();
+    println!("numrisks {} risks = {}", points.len(), risks);
+    Ok(())
+}
+
+fn findbasin(_m: &Matrix, _p: Point) -> Vec<Point> {
+    // Start with a point, see if everone up/down/left/right is lower.
+    // Add those points to the array if not already there.
+    // If array changed, recurse.
+    [(1, 2)].to_vec()
+}
+
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    let m = load(input)?;
+    let points = findlows(&m);
+    let mut sizes: Vec<isize> = Vec::new();
+    for p in points {
+        sizes.push(findbasin(&m, p).len() as isize);
+    }
+    sizes.sort();
+    sizes.reverse();
+    let sum: isize = sizes[0..3].iter().sum();
+    println!("sum = {}", sum);
+
+    Ok(())
 }
