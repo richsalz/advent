0a4b629e01fd9fd033f6023814fc8033d7b3ef45
Rich Salz <rsalz@akamai.com>
2022-01-03 18:59:39 -0500

    Start day10

---
+++ src/bin/d10.rs
@@ -0,0 +1,33 @@
+use advent::utils::Lines;
+use std::env;
+use std::error::Error;
+
+/// Loop over all arguments and process each one as a file.
+fn main() -> Result<(), Box<dyn Error>> {
+    for input in env::args().skip(1) {
+        part1(&input)?;
+        part2(&input)?;
+    }
+    Ok(())
+}
+
+
+const matchandscors: Vec<(char, char, i32)> = [
+( '(', ')', 3 ),
+( '[', ']', 57 ),
+( '{', '}', 1107 ),
+( '<', '>', 25137 ),
+];
+
+fn part1(input: &str) -> Result<(), Box<dyn Error>> {
+    let mut l = Lines::new(input)?;
+    while l.more() {
+
+    }
+    Ok(())
+}
+
+
+fn part2(_input: &str) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
