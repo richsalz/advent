1f1e5c0f2d63c4078549cd46c7025dcb9fdb28e8
Rich Salz <rsalz@akamai.com>
2022-01-05 18:31:40 -0500

    cargo fmt

--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 /// bracket we expect to see, and the cost if it doesn't match.
 fn match_closer(c: char) -> (char, usize) {
     match c {
-        ')' => return ( '(', 3 ),
-        ']' => return ( '[', 57 ),
-        '}' => return ( '{', 1197 ),
-        '>' => return ( '<', 25137 ),
+        ')' => return ('(', 3),
+        ']' => return ('[', 57),
+        '}' => return ('{', 1197),
+        '>' => return ('<', 25137),
         _ => panic!("unrecognized input character"),
     }
 }
@@ -27,14 +27,17 @@ fn corrupted(line: &str) -> Option<usize> {
     let mut stack: Vec<char> = Vec::new();
     for c in line.chars() {
         match c {
-            '(' | '[' | '{' | '<' => { stack.push(c); continue; },
+            '(' | '[' | '{' | '<' => {
+                stack.push(c);
+                continue;
+            }
             ')' | ']' | '}' | '>' => {
                 let (opener, value) = match_closer(c);
                 let stacktop = stack.pop();
                 if stacktop.is_none() || stacktop.unwrap() != opener {
                     return Some(value);
                 }
-            },
+            }
             _ => panic!("bad char in input"),
         }
     }
@@ -53,7 +56,6 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
 fn part2(input: &str) -> Result<(), Box<dyn Error>> {
     let mut l = Lines::new(input)?;
     let mut stack: Vec<char> = Vec::new();
@@ -67,20 +69,25 @@ fn part2(input: &str) -> Result<(), Box<dyn Error>> {
         for c in line.chars() {
             match c {
                 '(' | '[' | '{' | '<' => stack.push(c),
-                _ => {stack.pop(); () }
+                _ => {
+                    stack.pop();
+                    ()
+                }
             };
-        };
+        }
         stack.reverse();
         let mut sum: usize = 0;
         for c in stack.iter() {
-            sum = sum * 5 
-            + match c {
-                '(' => 1,
-                '[' => 2,
-                '{' => 3,
-                '<' => 4,
-                _ => { panic!("YABC {} in {}", c, line); }
-            };
+            sum = sum * 5
+                + match c {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => {
+                        panic!("YABC {} in {}", c, line);
+                    }
+                };
         }
         scores.push(sum);
     }
