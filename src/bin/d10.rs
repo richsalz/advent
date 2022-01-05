05737bcf494f409fd02a176838e32f2bb0eceb56
Rich Salz <rsalz@akamai.com>
2022-01-05 18:17:04 -0500

    part 2

--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -10,14 +10,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     Ok(())
 }
-/*
-
-const matchandscors: [(char, char, i32)] = [
-( '(', ')', 3 ),
-( '[', ']', 57 ),
-( '{', '}', 1107 ),
-( '<', '>', 25137 ),
-];*/
 
 /// Given the character `c` which is a closing bracket, return the opening
 /// bracket we expect to see, and the cost if it doesn't match.
@@ -30,28 +22,31 @@ fn match_closer(c: char) -> (char, usize) {
         _ => panic!("unrecognized input character"),
     }
 }
+
+fn corrupted(line: &str) -> Option<usize> {
+    let mut stack: Vec<char> = Vec::new();
+    for c in line.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => { stack.push(c); continue; },
+            ')' | ']' | '}' | '>' => {
+                let (opener, value) = match_closer(c);
+                let stacktop = stack.pop();
+                if stacktop.is_none() || stacktop.unwrap() != opener {
+                    return Some(value);
+                }
+            },
+            _ => panic!("bad char in input"),
+        }
+    }
+    return None;
+}
+
 fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     let mut l = Lines::new(input)?;
-    let mut stack: Vec<char> = Vec::new();
     let mut total: usize = 0;
     while l.more() {
-        stack.clear();
-        let line = l.get();
-        //println!("{}", line);
-        for c in line.chars() {
-            match c {
-                '(' | '[' | '{' | '<' => { stack.push(c); continue; },
-                ')' | ']' | '}' | '>' => {
-                    let (opener, value) = match_closer(c);
-                    let stacktop = stack.pop();
-                    if stacktop.is_none() || stacktop.unwrap() != opener {
-                        //println!("Mismatch; got {} wanted {} score {}", c, opener, value);
-                        total += value;
-                        break;
-                    }
-                },
-                _ => panic!("bad char in input"),
-            }
+        if let Some(value) = corrupted(l.get()) {
+            total += value;
         }
     }
     println!("total = {}", total);
@@ -59,6 +54,37 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
 }
 
 
-fn part2(_input: &str) -> Result<(), Box<dyn Error>> {
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    let mut l = Lines::new(input)?;
+    let mut stack: Vec<char> = Vec::new();
+    let mut scores: Vec<usize> = Vec::new();
+    while l.more() {
+        stack.clear();
+        let line = l.get();
+        if corrupted(line).is_some() {
+            continue;
+        }
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => stack.push(c),
+                _ => {stack.pop(); () }
+            };
+        };
+        stack.reverse();
+        let mut sum: usize = 0;
+        for c in stack.iter() {
+            sum = sum * 5 
+            + match c {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => { panic!("YABC {} in {}", c, line); }
+            };
+        }
+        scores.push(sum);
+    }
+    scores.sort();
+    println!("score = {}", scores[scores.len() / 2]);
     Ok(())
 }
