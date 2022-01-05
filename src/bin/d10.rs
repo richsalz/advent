30ebe1817ddf183e5fa8d4e793afb4ee9bee8848
Rich Salz <rsalz@akamai.com>
2022-01-04 20:08:09 -0500

    part 1 solved

--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -19,12 +19,42 @@ const matchandscors: [(char, char, i32)] = [
 ( '<', '>', 25137 ),
 ];*/
 
+/// Given the character `c` which is a closing bracket, return the opening
+/// bracket we expect to see, and the cost if it doesn't match.
+fn match_closer(c: char) -> (char, usize) {
+    match c {
+        ')' => return ( '(', 3 ),
+        ']' => return ( '[', 57 ),
+        '}' => return ( '{', 1197 ),
+        '>' => return ( '<', 25137 ),
+        _ => panic!("unrecognized input character"),
+    }
+}
 fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     let mut l = Lines::new(input)?;
+    let mut stack: Vec<char> = Vec::new();
+    let mut total: usize = 0;
     while l.more() {
-        let input = l.get().chars();
-        println!("{:?}", input);
+        stack.clear();
+        let line = l.get();
+        //println!("{}", line);
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => { stack.push(c); continue; },
+                ')' | ']' | '}' | '>' => {
+                    let (opener, value) = match_closer(c);
+                    let stacktop = stack.pop();
+                    if stacktop.is_none() || stacktop.unwrap() != opener {
+                        //println!("Mismatch; got {} wanted {} score {}", c, opener, value);
+                        total += value;
+                        break;
+                    }
+                },
+                _ => panic!("bad char in input"),
+            }
+        }
     }
+    println!("total = {}", total);
     Ok(())
 }
 
